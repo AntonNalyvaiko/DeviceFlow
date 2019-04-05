aae1994c8abe79116bc6008dc63941f9a7321dae
Anton <antonnalivayko@yahoo.com>
2019-04-05 21:31:10 +0300

    Init

---
+++ DeviceFlow.DeviceEmulator/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using IdentityModel;
+using IdentityModel.Client;
+
+namespace DeviceFlow.DeviceEmulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+            disco = new DiscoveryCache("https://localhost:44311");
+        
+        }
+
+        IDiscoveryCache disco;
+        
+        async Task<DeviceAuthorizationResponse> AuthorizationAsync()
+        {
+            var result = await disco.GetAsync();
+            if (result.IsError) throw new Exception(result.Error);
+
+            var client = new HttpClient();
+            var response = await client.RequestDeviceAuthorizationAsync(new DeviceAuthorizationRequest
+            {
+                Address = result.DeviceAuthorizationEndpoint,
+                ClientId = "device"
+            });
+
+            if (response.IsError) throw new Exception(response.Error);
+
+            TextUserCode.Text = $"user code : {response.UserCode}";
+            TextDeviceCode.Text = $"device code : {response.DeviceCode}";
+            TextURL.Text = $"URL : {response.VerificationUri}";
+            TextURLFull.Text = response.VerificationUriComplete;
+
+            return response;
+        }
+
+        private async Task<TokenResponse> RequestTokenAsync(DeviceAuthorizationResponse authorizeResponse)
+        {
+            var result = await disco.GetAsync();
+            if (result.IsError) throw new Exception(result.Error);
+
+            var client = new HttpClient();
+
+            while (true)
+            {
+                var response = await client.RequestDeviceTokenAsync(new DeviceTokenRequest
+                {
+                    Address = result.TokenEndpoint,
+                    ClientId = "device",
+                    DeviceCode = authorizeResponse.DeviceCode
+                });
+
+                if (response.IsError)
+                {
+                    if (response.Error == OidcConstants.TokenErrors.AuthorizationPending ||
+                        response.Error == OidcConstants.TokenErrors.SlowDown)
+                        await Task.Delay(authorizeResponse.Interval * 1000);
+                    else
+                        throw new Exception(response.Error);
+                }
+                else
+                    return response;
+            }
+        }
+
+        async Task CallApiAsync(string token)
+        {
+            var baseAddress = "https://localhost:44312";
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = await client.GetAsync("api/values");
+            Textresult.Text ="Result is :"+ await response.Content.ReadAsStringAsync();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var authorizeResponse = await AuthorizationAsync();
+            var tokenResponse = await RequestTokenAsync(authorizeResponse);
+            await CallApiAsync(tokenResponse.AccessToken);
+        }
+    }
+}
---
+++ DeviceFlow.Idp/Config.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace DeviceFlow.Idp
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "user",
+                    Password = "password"
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "device",
+                    EnableLocalLogin = true,
+                    ClientName = "Device Client",
+                    AllowedGrantTypes = GrantTypes.DeviceFlow,
+                    RequireClientSecret = false,
+
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    AllowOfflineAccess = true,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                }
+            };
+        }
+    }
+}
